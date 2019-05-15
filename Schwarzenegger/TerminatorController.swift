b16c8cf0092cf9a325bd6dabd2d5a15e5daf0d7d
Leonardo Kaminski Ferreira <leonardo@mindvalley.com>
2019-05-15 16:52:35 -0300

    Adds project

---
+++ Schwarzenegger/TerminatorController.swift
@@ -0,0 +1,65 @@
+import Cocoa
+
+@objc public final class TerminatorController: NSViewController {
+
+    /// Hosts an array of `SystemProcess` instances.
+    /// You can find a list of the possible actions on an instance
+    /// in the `SystemProcess` class
+    @IBOutlet var processesArrayController: NSArrayController?
+    private var reloadTimer: Timer?
+
+    public override func viewWillAppear() {
+        super.viewWillAppear()
+        reload()
+    }
+
+    // MARK: IB Actions
+
+    @IBAction func terminateAllSelected(sender: AnyObject) {
+        terminateAllSelected(force: false)
+    }
+
+    @IBAction func forceTerminateAllSelected(sender: AnyObject) {
+        terminateAllSelected(force: true)
+    }
+    
+    @IBAction func hideAllSelected(sender: AnyObject) {
+        hideAllSelected(true)
+    }
+    
+    @IBAction func unhideAllSelected(sender: AnyObject) {
+        hideAllSelected(false)
+    }
+
+    // MARK: Private
+
+    private func terminateAllSelected(force: Bool) {
+        guard let processes = processesArrayController?.selectedObjects as? [SystemProcess] else { return }
+        processes.forEach { process in
+            if force {
+                process.forceTerminate()
+            } else {
+                process.terminate()
+            }
+        }
+    }
+    
+    private func hideAllSelected(_ hide: Bool) {
+        guard let processes = processesArrayController?.selectedObjects as? [SystemProcess] else { return }
+        processes.forEach { process in
+            if hide {
+                process.hide()
+            } else {
+                process.unhide()
+            }
+        }
+    }
+
+    private func reload() {
+        reloadTimer?.invalidate()
+        processesArrayController?.content = SystemProcess.processes
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { [weak self] _ in
+            self?.reload()
+        }
+    }
+}
